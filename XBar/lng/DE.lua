d5081ea21842bb622e160841f1d898d7e05f02d4
matiffeder <matif.tw@yahoo.com.tw>
2023-04-01 19:21:18 -0400

    back up from https://www.curseforge.com/

---
+++ XBar/lng/DE.lua
@@ -0,0 +1,152 @@
+XBar.Lng = {
+	["Ttip"]={
+		["CfgBtn1"]="XBar Konfiguration",
+		["CfgBtn2"]="Links-Klick öffnet das Konfigurations-Menü.\nRechts-Klick öffnet das Addons-Menü.\nMittel-Klick autom. Laufen an/aus.",
+		["SysBtn1"]="Funktionen",
+		["SysBtn2"]="Links-Klick öffnet das Haupt-Menu.\nRechts-Klick öffnet das System-Menu.\nMittel-Klick führt ReloadUI() aus.",
+		["DspBtn1"]="Links-Klick öffnet das Transport-Buch.\nRechts-Klick für die Weltkarte.\nMittel-Klick setzt Anzeige auf niedrige Qualitat.",
+		["SndBtn1"]="Links-Klick öffnet die Weltsuche.\nRechts-Klick öffnet den Instanzen-Fortschritt.\nMittel-Klick schaltet alle Sounds aus.",
+
+		["Ammo1"]="Links-Klick öffnet den Quest-Buch.\nRechts-Klick öffnet das Karten-Kompendium.",
+
+		["Bag1"]="Links-Klick öffnet den Rucksack.\nRechts-Klick öffnet die Bank oder die Tragbare Bank.",
+
+		["Class1"]="Links-Klick öffnet das Charakterfenster.\nRechts-Klick öffnet Set-Fertigkeiten.\nMittel-Klick öffnet die Titelauswahl.\nStrg + Rechts-Klick entfernt Titel.",
+
+		["Clock1"]="Links-Klick für 12H / 24H Modus.\nRechts-Klick für Zeit / Datum.\nShift-Klick für die heutige / gesamte Spielzeit.",
+		["Clock2"]="Gesamt-Spielzeit",
+
+		["Craft1"]="Handwerk",
+		["Craft2"]="Links-Klick öffnet den Arkanen Umwandler.\nRechts-Klick öffnet die Runen-Extraktion.",
+		["Craft3"]="letzte / Wiederholung",
+		["Craft4"]="Count",
+
+		["Dps1"]="DPS / Haltbarkeit / Fähigkeiten",
+		["Dps2"]="Links-Klick öffnet die Rüstungsvorschau.\nRechts-Klick für Ausrüstung wechseln.",
+
+		["Exp1"]="Erfahrung / Schulden",
+		["Exp2"]="Links-Klick öffnet das Fertigkeits-Fenster.\nRechts-Klick Handwerk.",
+		["Exp3"]="aktuell",
+		["Exp4"]="benötigt",
+		["Exp5"]="letztes",
+		["Exp6"]="Wiederholungen",
+		["Exp7"]="aktuelle TP",
+
+		["Friend1"]="Links-Klick zeigt online Freunde an.\nRechts-Klick Freundeliste.",
+		["Friend2"]="Freunde online",
+		["Friend3"]="Gruppe :",
+
+		["Guild1"]="Links-Klick zeigt online Mitglieder an.\nRechts-Klick öffnet das Gildenfenster.",
+		["Guild2"]="Mitglieder",
+
+		["Appointment"]="Nicht registriert",
+		["Failed"]="Kein Gegner",
+		["Disable"]="|cffFFAA00BK angemeldet|r",
+		["Prepare"]="|cffFFAA00BK Vorbereitung|r",
+		["Fight"]="|cffFFAA00BK läuft! Bitte eintreten!|r",
+		["Fighting"]="|cffFFAA00BK läuft! Im Kampf!|r",
+		["FightEnd"]="BK beendet",
+
+		["Mail1"]="eingestellte / verkaufte Items im AH",
+		["Mail2"]="Links-Klick öffnet das Postfach.\nRechts-Klick die Partnertasche.",
+		["Mail3"]="verkauft",
+
+		["Money1"]="Links-Klick öffnet den Item-Shop Rucksack.\nRechts-Klick den Item Shop.",
+		["Money2"]="verdient",
+		["Money3"]="bezahlt",
+		["Money4"]="Bilanz",
+
+		["Ping1"]="Leistung",
+		["Ping2"]="Links-Klick öffnet das Transport-Buch.",
+		["Ping3"]="Rechts-Klick öffnet die Weltsuche.",
+
+		["EQDmg1"]="Text ändern",
+		["EQDmg2"]="- Benutze dieses Fenster um Ausrüstung zu reparieren oder zu wechseln.\n- |cffFFAA00Links-Klick|r um den Reperaturhammer zu benutzen der rechts oben angezeigt wird, falls du einen hast.",
+		["AvgDur"]="Haltbarkeit",
+		["LowDur"]="Niedrigste Haltbarkeit",
+
+		["CR_REST"]="Strg + Rechts-Klick um zurück zu setzen ",
+		["LMOUSE_WSP"]="Links-Klick zum anflüstern.",
+		["RMOUSE_INV"]="Rechts-Klick für Partie-Einladung.",
+		["SCROLL_ON"]="Scrollen aktiviert.",
+		["Loc"]="Aufenthaltsort :",
+		["Note"]="Bemerkung :",
+		["Sys_SC1"]="Charakter wählen",
+	},
+	["Config"]={
+		["saved"]="|cffFFE855XBar|r Einstellungen gespeichert.",
+		["help"]="XBar Hilfe:",
+		["toggle_xbar"]="|cffFFE855/xb on|r - Schaltet XBar an oder aus.",
+		["toggle_main"]="|cffFFE855/xb main|r - Versteckt das MainMenuFrame oder blendet es ein.",
+		["toggle_expf"]="|cffFFE855/xb exp|r - Zeigt oder versteckt den Erfahrungsbalken.",
+		["loaded1"]="|cffFFE855XBar [DE] ",
+		["loaded2"]="|r geladen, |cffFFE855/xbar|r eingeben um Einstellungen zu öffnen.",
+		["Title"]="XBar Addon Management System",
+		["Descr"]="nigrangelus: Ich schrieb dies um die XBar Konfiguration zu erweitern.\nEs erlaubt jedem Addon ein Teil dieses zusein",
+		["Info"]="Shift + Rechts-Klick um XBar Buttons zu bewegen.\nMausrad auf Chatfenster zum scrollen.\nDu kannst Button-Dateien löschen die Du nicht brauchst.\nNach dem Löschen des Buttons bitte Spiel neu starten.",
+		["EnableXBar"]="aktiviere XBar",
+		["Top"]="oben",
+		["Bottom"]="unten",
+		["Main4"]="zeige alle Haupt-Buttons",
+		["Main2"]="zeige nur 2 Haupt-Buttons",
+		["Main0"]="verstecke alle Haupt-Buttons",
+		["Style"]="Style",
+		["HideOriginalXP"]="verstecke original Erfahrungs-Fenster",
+		["HideOriginalED"]="verstecke original EquipDamage Fenster",
+		["Text1"]="Textfeld 1",
+		["Text2"]="Textfeld 2",
+		["Value"]="Inhalt (max 255 Zeichen)",
+		["ValueL1"]="Inhalt A: (max 255 Zeichen) |cffFFE855[L-Klick]|r",
+		["ValueL2"]="Inhalt B: (max 255 Zeichen) |cffFFE855[L-Klick]|r",
+		["ValueR"]="Inhalt C: (max 255 Zeichen) |cffFFE855[R-Klick]|r",
+		["ValueSL1"]="Inhalt A: (max 255 Zeichen) |cffFFE855[Shift + L-Klick]|r",
+		["ValueSL2"]="Inhalt B: (max 255 Zeichen) |cffFFE855[Shift + L-Klick]|r",
+		["Head"]="|cffFFE855Schlüsselworte Ersetzt|r\n\n",
+
+		["Count"]="Anzahl der %s",
+		["Name"]="Name der %s",
+
+		["Bag1"]="Anzahl belegter %s",
+		["Bag2"]="Die Anzahl der max verfügbaren Plätze",
+		["Bag3"]="Prozentuale Auslastung der %s",
+
+		["Class1"]="Dritt-Klasse",
+		["Class2"]="%s Level",
+		["Class3"]="abgeschlossene Quests",
+
+		["Clock1"]="Spielzeit seit Login",
+		["Clock2"]="Gesamt-Spielzeit über Tage",
+
+		["Craft1"]="%s Lv",
+		["Craft2"]="%s XP",
+		["Craft3"]="Anzahl %s-Wiederholungen bis zum nächsten Level",
+		["Craft4"]="Letzte %s XP",
+
+		["Dps1"]="Berechneter %s pro Sekunde",
+		["Dps2"]="Letzte %s",
+		["Dps3"]="Getötete Monster",
+
+		["Exp1"]="Benötigte Erfahrung bis LvlUp",
+		["Exp2"]="Gesamte Erfahrung für dieses Level",
+		["Exp3"]="Aktuelle XP in %",
+		["Exp4"]="Benötigte Erfahrung in %",
+		["Exp5"]="Benötigte Kills zum LvlUp",
+		["Exp6"]="letzten gewonnene %s",
+		["Exp7"]="%s in dieser Sitzung",
+		["Exp8"]="gesamte TP",
+		["Exp9"]="benutzte TP",
+		["Exp10"]="%s Hausbonus",
+		["Exp11"]="%s Schulden",
+		["Exp12"]="%s XP",
+		["Exp13"]="%s max XP",
+
+		["Friend1"]="Anzahl der Online-Freunde",
+		["Friend2"]="Anzahl der Freunde gesamt",
+
+		["Guild1"]="Derzeitige Anzahl der Online-Mitglieder",
+		["Guild2"]="Gesamtzahl der Gilden-Mitglieder",
+		["Guild3"]="Max. Mitglieder Anzahl für dieses Gilden-Level",
+
+		["Mail1"]="Erhaltene Mails",
+	},
+}
